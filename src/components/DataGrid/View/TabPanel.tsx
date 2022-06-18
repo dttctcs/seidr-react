a12e8164107a3bc3b2cf8b59b40e448ec4db1e83
Mimameid <ibrahimkeser@outlook.de>
2022-06-18 23:17:22 +0200

    Minor bugfix

--- src/components/DataGrid/View/TabPanel.tsx
+++ src/components/DataGrid/View/TabPanel.tsx
@@ -1,31 +1,30 @@
-import React from "react";
+import React from 'react';
 
-import { Box, Paper } from "@mui/material";
+import { Box, Paper } from '@mui/material';
 
 function TabPanel({ children, value, index }) {
   return (
     value === index && (
-      <Box sx={{ display: "flex", flexDirection: "column", flex: 1, px: 0, overflow: "auto" }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1, px: 0, overflow: 'auto' }}>
         <Paper
           sx={{
-            display: "flex",
-            flexDirection: "column",
+            display: 'flex',
+            flexDirection: 'column',
             flex: 1,
-            overflow: "hidden",
-            borderWidth: "1px",
-            borderStyle: "solid",
+            overflow: 'hidden',
+            borderWidth: '1px',
+            borderStyle: 'solid',
             borderColor: (theme) => theme.palette.primary.main + 55,
 
-            "&:hover": {
+            '&:hover': {
               boxShadow: (theme) => theme.shadows[3],
             },
-            ...sx,
           }}
           role="tabpanel"
           hidden={value !== index}
           elevation={0}
         >
-          <Box sx={{ flex: 1, display: "flex", flexDirection: "column", overflow: "auto" }}>{children}</Box>
+          <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', overflow: 'auto' }}>{children}</Box>
         </Paper>
       </Box>
     )
