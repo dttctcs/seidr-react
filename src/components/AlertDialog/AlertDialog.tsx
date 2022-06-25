00d4d1fd48ba4bf2466d82ebff063921c4512305
Mimameid <ibrahimkeser@outlook.de>
2022-06-25 17:20:53 +0200

    AlertDialog is now created with mantine components

--- src/components/AlertDialog/AlertDialog.tsx
+++ src/components/AlertDialog/AlertDialog.tsx
@@ -1,39 +1,26 @@
 import React from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 
-export function AlertDialog({ open, onClose, handleAccept, handleReject }) {
+import { Button, Group, Modal } from '@mantine/core';
+
+export function AlertDialog({ opened, onClose, handleAccept, handleReject }) {
   return (
-    <Dialog
-      open={open}
+    <Modal
+      opened={opened}
       onClose={onClose}
-      PaperProps={{
-        sx: {
-          width: 1,
-          maxWidth: '480px',
-          p: 2,
-          borderWidth: '1px',
-          borderStyle: 'solid',
-          borderColor: (theme) => theme.palette.primary.main + 44,
-        },
-        elevation: 0,
-      }}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
+      title="Item löschen?"
+      centered
     >
-      <DialogTitle id="alert-dialog-title">{'Item löschen?'}</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          Sind Sie Sicher, dass Sie das Item löschen wollen?
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button color="inherit" onClick={handleReject}>
+      Sind Sie Sicher, dass Sie das Item löschen wollen?
+      <Group position="right" mt="xl">
+        <Button onClick={handleReject} variant="default">
           Abbrechen
         </Button>
-        <Button color="error" onClick={handleAccept} variant="contained">
+        <Button onClick={handleAccept} color="red">
           Löschen
         </Button>
-      </DialogActions>
-    </Dialog>
+      </Group>
+    </Modal>
   );
 }
