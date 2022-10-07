e5c40e920cb0d05a3e6343c8473b23e8a8b3ec76
Mimameid <ibrahimkeser@outlook.de>
2022-10-07 19:27:42 +0200

    Updated github workflow

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,6 +1,4 @@
-import path from 'path';
 import chalk from 'chalk';
-import simpleGit from 'simple-git';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 import { setPackageVersion } from './utils/set-package-version';
@@ -8,7 +6,6 @@ import { setPackageVersion } from './utils/set-package-version';
 import { Logger } from './utils/Logger';
 
 const logger = new Logger('build');
-const git = simpleGit();
 
 const { argv }: { argv: any } = yargs(hideBin(process.argv)).option('tag', {
   type: 'string',
@@ -24,17 +21,4 @@ const { argv }: { argv: any } = yargs(hideBin(process.argv)).option('tag', {
   logger.info(`Updating package.json version...`);
   await setPackageVersion(version);
   logger.success(`Updated package.json version: ${chalk.cyan(version)}`);
-
-  await git.add([path.join(__dirname, '..')]);
-  await git.commit(`[release] Version: ${version}`);
-  await git.push();
-
-  // open(
-  //   githubRelease({
-  //     user: 'dttctcs',
-  //     repo: 'seidr-react',
-  //     tag: incrementedVersion,
-  //     title: incrementedVersion,
-  //   }),
-  // );
 })();
