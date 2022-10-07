48ee2a68e8c000ba99f9fb5aeba77a18f3a40765
Mimameid <ibrahimkeser@outlook.de>
2022-10-07 19:20:26 +0200

    Updated github workflow

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,15 +1,11 @@
-import open from 'open';
 import path from 'path';
 import chalk from 'chalk';
 import simpleGit from 'simple-git';
-import githubRelease from 'new-github-release-url';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 import { setPackageVersion } from './utils/set-package-version';
-import { getIncrementedVersion } from './utils/get-incremented-version';
 
 import { Logger } from './utils/Logger';
-import packageJson from '../package.json';
 
 const logger = new Logger('build');
 const git = simpleGit();
@@ -29,9 +25,9 @@ const { argv }: { argv: any } = yargs(hideBin(process.argv)).option('tag', {
   await setPackageVersion(version);
   logger.success(`Updated package.json version: ${chalk.cyan(version)}`);
 
-  // await git.add([path.join(__dirname, '..')]);
-  // await git.commit(`[release] Version: ${incrementedVersion}`);
-  // await git.push();
+  await git.add([path.join(__dirname, '..')]);
+  await git.commit(`[release] Version: ${version}`);
+  await git.push();
 
   // open(
   //   githubRelease({
