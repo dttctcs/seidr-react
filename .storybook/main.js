0afd88edbf0088cab37277d9d3d14605232366f0
Mohr, Thomas (Datatactics) <t.mohr@datatactics.de>
2023-04-06 15:44:19 +0200

    Update dependencies
    * Fixes for storybook 7 migration
    * Fixes for msw 1 migration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,25 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  core: {
-    builder: 'webpack5',
-  },
   addons: [
     '@storybook/addon-controls',
     '@storybook/addon-a11y',
     '@storybook/addon-actions',
-
     '@storybook/addon-backgrounds',
     '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-toolbars',
     '@storybook/addon-viewport',
   ],
-  framework: '@storybook/react',
-  reactOptions: {
-    fastRefresh: true,
-    // strictMode: true, // causes issues
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      fastRefresh: true,
+    },
   },
   features: {
     previewMdx2: true,
+  },
+  docs: {
+    autodocs: true,
   },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,26 +1,31 @@
-import { setupWorker } from 'msw';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
 import { handlers } from '../src/mocks/handlers';
 import { SeidrProvider } from '../src';
 
-if (typeof global.process === 'undefined') {
-  const worker = setupWorker(...handlers);
-  worker.start({ onUnhandledRequest: 'bypass' });
-}
+initialize();
 
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+// if (typeof global.process === 'undefined') {
+//   const worker = setupWorker(...handlers);
+//   worker.start({ onUnhandledRequest: 'bypass' });
+// }
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
   },
 };
+export default preview;
 
 export const decorators = [
   (Story) => {
     return (
-      <SeidrProvider baseUrl="http://localhost:6060/api/v1">
+      <SeidrProvider baseUrl='http://localhost:6060/api/v1'>
         <Story />
       </SeidrProvider>
     );
