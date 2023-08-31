fa78dbfd606781bdd57327a016a2641caa4bafcd
Patrick Heusinger <p.heusinger@datatactics.de>
2023-08-31 15:26:49 +0200

    clean up

--- src/utils/filterProps.js
+++
@@ -1,8 +0,0 @@
-export function filterProps(props) {
-  return Object.keys(props).reduce((acc, key) => {
-    if (props[key] !== undefined) {
-      acc[key] = props[key];
-    }
-    return acc;
-  }, {});
-}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,2 @@
 export { createFetchParams } from './createFetchParams';
-export { filterProps } from './filterProps';
 export { urlJoin, normalize } from './urlJoin';
