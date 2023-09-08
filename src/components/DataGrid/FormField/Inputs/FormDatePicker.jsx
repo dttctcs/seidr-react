942078631bc378f5a06359e63b32847dc99c5007
Patrick Heusinger <p.heusinger@datatactics.de>
2023-09-08 14:17:43 +0200

    init mantine datetimepicker

--- src/components/DataGrid/FormField/Inputs/FormDatePicker.jsx
+++ src/components/DataGrid/FormField/Inputs/FormDatePicker.jsx
@@ -7,19 +7,40 @@ export function FormDatePicker({ control, name, ...props }) {
     control,
   });
 
+
   return (
     <>
       <DatePicker
         withinPortal
+       /* getDayProps={(date) => {
+          const today = new Date();
+          if (
+            date.getDate() === today.getDate() &&
+            date.getMonth() === today.getMonth() &&
+            date.getFullYear() === today.getFullYear()
+          ) {
+            return {
+              sx: (theme) => ({
+                backgroundColor: theme.colors.red[theme.fn.primaryShade()],
+                color: theme.white,
+                ...theme.fn.hover({ backgroundColor: theme.colors.red[7] }),
+              }),
+            };
+          }
+
+          return {};
+        }}*/
         onChange={(newValue) => {
           if (newValue instanceof Date) {
             const utcDate = new Date(newValue.getTime() - newValue.getTimezoneOffset() * 60000);
             field.onChange(utcDate.toISOString().substring(0, 10));
+            {console.log(utcDate)}
           }
         }}
         value={field.value ? new Date(field.value) : null}
         {...props}
       />
+
     </>
   );
 }
--- src/components/DataGrid/FormField/Inputs/FormDateTimePicker.jsx
+++ src/components/DataGrid/FormField/Inputs/FormDateTimePicker.jsx
@@ -1,9 +1,5 @@
 import { useController } from 'react-hook-form';
-
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
+import { DateTimePicker } from '@mantine/dates';
 
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
@@ -11,41 +7,46 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 
 export function FormDateTimePicker({ control, name, ...props }) {
-  const {
-    field,
-    fieldState,
-  } = useController({
+  const { field } = useController({
     name,
     control,
   });
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <>
       <DateTimePicker
-        ampm={false}
-        viewRenderers={{
-          hours: renderTimeViewClock,
-          minutes: renderTimeViewClock,
-          seconds: renderTimeViewClock,
-        }}
-        slotProps={{
-          popper: {},
-          textField: {
-            autoComplete: 'off',
-            label: props.label,
-            error: !!fieldState.error,
-          },
-        }}
-        inputRef={field.ref}
-        onChange={(newValue) => {
-          if (newValue instanceof dayjs && !isNaN(newValue)) {
-            return field.onChange(newValue.toISOString());
-          }
-          return field.onChange(null);
-        }}
-        value={dayjs.utc(field.value)}
-        {...props}
+        valueFormat="DD MMM YYYY hh:mm A"
+        maw={400}
+        mx="auto"
       />
-    </LocalizationProvider>
+    </>
   );
 }
+
+/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+     <DateTimePicker
+       ampm={false}
+       viewRenderers={{
+         hours: renderTimeViewClock,
+         minutes: renderTimeViewClock,
+         seconds: renderTimeViewClock,
+       }}
+       slotProps={{
+         popper: {},
+         textField: {
+           autoComplete: 'off',
+           label: props.label,
+           error: !!fieldState.error,
+         },
+       }}
+       inputRef={field.ref}
+       onChange={(newValue) => {
+         if (newValue instanceof dayjs && !isNaN(newValue)) {
+           return field.onChange(newValue.toISOString());
+         }
+         return field.onChange(null);
+       }}
+       value={dayjs.utc(field.value)}
+       {...props}
+     />
+   </LocalizationProvider>*/
