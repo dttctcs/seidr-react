5d4f9f8764c83fc8743dea715c4614135f1957b7
Patrick Heusinger <p.heusinger@datatactics.de>
2023-11-13 14:07:04 +0100

    migrate date time picker mantine

--- src/components/DataGrid/FormField/Inputs/FormDateTimePicker.jsx
+++ src/components/DataGrid/FormField/Inputs/FormDateTimePicker.jsx
@@ -1,57 +1,3 @@
-/*
-import { useController } from 'react-hook-form';
-
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
-
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-
-dayjs.extend(utc);
-
-export function FormDateTimePicker({ control, name, ...props }) {
-  const {
-    field,
-    fieldState,
-  } = useController({
-    name,
-    control,
-  });
-
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateTimePicker
-        ampm={false}
-        viewRenderers={{
-          hours: renderTimeViewClock,
-          minutes: renderTimeViewClock,
-          seconds: renderTimeViewClock,
-        }}
-        slotProps={{
-          popper: {},
-          textField: {
-            autoComplete: 'off',
-            label: props.label,
-            error: !!fieldState.error,
-          },
-        }}
-        inputRef={field.ref}
-        onChange={(newValue) => {
-          if (newValue instanceof dayjs && !isNaN(newValue)) {
-            return field.onChange(newValue.toISOString());
-          }
-          return field.onChange(null);
-        }}
-        value={dayjs.utc(field.value)}
-        {...props}
-      />
-    </LocalizationProvider>
-  );
-}
-*/
-
 import { useController } from 'react-hook-form';
 import { DateTimePicker } from '@mantine/dates';
 
@@ -61,15 +7,14 @@ export function FormDateTimePicker({ control, name, ...props }) {
     control,
   });
 
-
   return (
     <>
       <DateTimePicker
         withinPortal
         onChange={(newValue) => {
           if (newValue instanceof Date) {
-            const utcDate = new Date(newValue.getTime() - newValue.getTimezoneOffset() * 60000);
-            field.onChange(utcDate.toISOString().substring(0, 10));
+            const utcDate = new Date(newValue);
+            field.onChange(utcDate.toISOString());
           }
         }}
         value={field.value ? new Date(field.value) : null}
