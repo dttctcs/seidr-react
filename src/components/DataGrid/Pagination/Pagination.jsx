8fa77c24ffffaca253e30c4d83df918dfc320585
bernhardrp <b.putranto@datatactics.de>
2023-11-16 09:39:52 +0100

    Migrate pagination component

--- src/components/DataGrid/Pagination/Pagination.jsx
+++ src/components/DataGrid/Pagination/Pagination.jsx
@@ -1,62 +1,21 @@
-import { useEffect, useMemo, useState, memo} from 'react';
+import { useEffect, useMemo, useState, memo } from 'react';
 import { useApi } from '../../SeidrApiProvider';
 
-import { TablePagination, debounce } from '@mui/material';
-import { Box, ActionIcon, Group } from '@mantine/core';
-
-import { IconArrowBarToLeft, IconArrowBarToRight, IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
-
-function DataGridPaginationActions(props) {
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <Group ml='sm' noWrap>
-      <ActionIcon onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label='first page'>
-        <IconArrowBarToLeft />
-      </ActionIcon>
-      <ActionIcon onClick={handleBackButtonClick} disabled={page === 0} aria-label='previous page'>
-        <IconChevronLeft />
-      </ActionIcon>
-      <ActionIcon
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label='next page'
-      >
-        <IconChevronRight />
-      </ActionIcon>
-      <ActionIcon
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label='last page'
-      >
-        <IconArrowBarToRight />
-      </ActionIcon>
-    </Group>
-  );
-}
+import { Box, Pagination as MantinePagination } from '@mantine/core';
 
 export const Pagination = memo(({ ...props }) => {
   const { data, queryParams, setQueryParams } = useApi();
-  const pageSizes = [10, 25, 50];
   const rowsPerPage = queryParams.page_size;
   const [cachedPage, setCachedPage] = useState(queryParams.page);
-
+  
+  const debounce = (func, delay) => {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => func.apply(this, args), delay);
+    };
+  };
+  
   const debouncedSetPage = useMemo(
     () =>
       debounce((val) => {
@@ -69,14 +28,9 @@ export const Pagination = memo(({ ...props }) => {
     setCachedPage(queryParams.page);
   }, [queryParams.page]);
 
-
-  const handlePageChange = (event, newPage) => {
-    setCachedPage(newPage);
-    debouncedSetPage(newPage);
-  };
-
-  const handleRowsPerPageChange = (event) => {
-    setQueryParams({ page_size: parseInt(event.target.value, 10) });
+  const handlePageChange = (newPage) => {
+    setCachedPage(newPage-1);
+    debouncedSetPage(newPage-1);
   };
 
   return (
@@ -88,23 +42,14 @@ export const Pagination = memo(({ ...props }) => {
         borderTopWidth: '1px',
         borderTopStyle: 'solid',
         borderColor: theme.colors.gray[4],
+        padding: theme.spacing.md,
       })}
     >
-      <TablePagination
-        sx={{
-          color: 'inherit',
-          '& .MuiSvgIcon-root': {
-            color: 'inherit',
-          },
-        }}
-        rowsPerPageOptions={pageSizes}
-        component='div'
-        count={data.count}
-        rowsPerPage={rowsPerPage}
-        page={cachedPage}
-        onPageChange={handlePageChange}
-        onRowsPerPageChange={handleRowsPerPageChange}
-        ActionsComponent={DataGridPaginationActions}
+      <MantinePagination
+        position='center'
+        total={Math.ceil(data.count/rowsPerPage)}
+        value={cachedPage + 1}
+        onChange={handlePageChange}
         {...props}
       />
     </Box>
