0d5a4026d1059cde11c9e791c259bed4c94779ec
bernhardrp <b.putranto@datatactics.de>
2023-11-23 15:22:33 +0100

    Remove unsupported style for Mantine 7

--- src/components/DataGrid/index.js
+++ src/components/DataGrid/index.js
@@ -1,4 +1,3 @@
 export { DataGrid } from './DataGrid';
-export { DataGridStyles } from './DataGrid.styles';
 export { Actions, Add, AddDialog, Delete, Edit, EditDialog } from './Actions';
 export { Filter, FilterMenu } from './Toolbar';
