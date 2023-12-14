b751c8b02afa1cbb682dd39bd94033202070d238
bernhardrp <b.putranto@datatactics.de>
2023-12-14 16:14:52 +0100

    Add abort controller

--- src/components/SeidrApiProvider/utils/api/getList.js
+++ src/components/SeidrApiProvider/utils/api/getList.js
@@ -1,6 +1,14 @@
 import { createFetchParams, urlJoin } from '../../../../utils';
 
+export let controller = null;
+
 export const getList = async (path, queryParams) => {
+  if (controller) {
+    controller.abort();
+  }
+  controller = new AbortController();
+  const signal = controller.signal;
+  
   try {
     const { fetchPath, options } = createFetchParams({
       path: urlJoin(path, '/'),
@@ -8,8 +16,7 @@ export const getList = async (path, queryParams) => {
       //queryParams: { q: encodeURIComponent(JSON.stringify(queryParams)) },
       queryParams: { q: JSON.stringify(queryParams) },
     });
-
-    const response = await fetch(fetchPath, options);
+    const response = await fetch(fetchPath, {...options, signal});
     if (response.ok) {
       const data = await response.json();
       return data;
@@ -17,6 +24,6 @@ export const getList = async (path, queryParams) => {
 
     throw new Error(response.statusText);
   } catch (error) {
-    throw new Error(error);
+    if(!error instanceof AbortSignal) throw new Error(error);
   }
 };
